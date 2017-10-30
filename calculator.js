a51f2b8673615539990c850e543fca3b12f3d902
Nadya <nadia_1492@mail.ru>
2017-10-30 23:25:17 +0300

    home changes
    fixed issue#1

--- calculator.js
+++ calculator.js
@@ -1,49 +1,56 @@
 function calculator(){
     var buffer = 0;
+    var checkValue = function(value){
+        if (value === undefined || value === null || typeof value === string || 
+            typeof value === array ||typeof value === object ){
+                return false;
+            }
+            else return true;
+    }
     return {
         add : function (value1){
+            buffer += value1;
             return function(value2){
-                if (value2){
-                    return buffer = value1 + value2;
+                if (checkValue(value2)){
+                    return buffer += value2;
                 }
-                else {
-                    return buffer += value1;
-                }
+                    else return buffer;
             }
         }, 
-        deduct : function (value1){
+        subtract : function (value1){
+            buffer -= value1;
             return function(value2){
-                if (value2){
-                    return buffer = value1 - value2;
+                if (checkValue(value2)){
+                    return buffer -= value2;
                 }
-                else {
-                    return buffer -= value1;
-                }
+                    else return buffer;
             }
         }, 
         multiply : function (value1){
+            buffer *= value1;
             return function(value2){
-                if (value2){
-                    return buffer = value1 * value2;
+                if (checkValue(value2)){
+                    return buffer *= value2;
                 }
-                else {
-                    return buffer *= value1;
-                }
+                    else return buffer;
             }
         }, 
         divide : function (value1){
+            buffer /= value1;
             return function(value2){
-                if (value2){
-                    return buffer = value1 / value2;
+                if (checkValue(value2)){
+                    return buffer /= value2;
                 }
-                else {
-                    return buffer /= value1;
-                }
+                    else return buffer;
             }
+        },
+        getResult : function(){
+            return buffer;
         } 
     }
 }
 
 var calc = new calculator();
-console.log(calc.deduct(10)(8));
-console.log(calc.divide(2)());
+console.log(calc.add(10)());
+console.log(calc.subtract(5));
+console.log(calc.getResult());
