d5adc0967e9560d5ca651d650d5dbdc13121377b
Nadya <nadia_1492@mail.ru>
2017-11-26 23:04:40 +0300

    dom task1 v1

--- styles.js
+++ styles.js
@@ -1,14 +1,29 @@
 var styleObject = {
     body : {
     margin: '0px',
-    position: "relative"
+    position: "relative",
+    fontFamily: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
+  },
+  "menu-content":{
+    display: "flex"
   }
 }
 
-var pageBody = document.getElementsByTagName('body');
+function loadingStyles(){
+    var pageBody = document.getElementsByTagName("body");
+    for(var key in styleObject.body){
+        pageBody[0].style[key] = styleObject.body[key];
+    }    
 
-for(var key in styleObject["body"]){
-    pageBody.style[key] = pageBody[key];
+    
+var menu = document.getElementsByClassName("menu-content");
+menu.addEventListener('click', function(){
+    menu.style.display = styleObject["menu-content"].display;
+});
 }
+
+
+
+
 
 
