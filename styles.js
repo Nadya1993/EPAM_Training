edbbaa8a4c90ebea2c443cfee3bb5e4eb66e1ed7
Nadya <nadia_1492@mail.ru>
2017-11-28 00:09:05 +0300

    dom task1 v2

--- styles.js
+++ styles.js
@@ -4,23 +4,62 @@ var styleObject = {
     position: "relative",
     fontFamily: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
   },
-  "menu-content":{
-    display: "flex"
+  "menu-content" : {
+    "display": "flex",
+    "flex-direction": "column",
+    "background-color": "#FFFFE0",
+    "min-width": "280px",
+    "margin-top": "5px",
+    "box-shadow": "0px 8px 16px 0px rgba(0,0,0,0.2)"
   }
 }
+
+function hoverMenuItem(event) {
+    this.style.backgroundColor = "#FFDAB9";
+    if (event.relatedTarget.className != "menu" && 
+        event.relatedTarget.className != "menu-btn" &&
+        event.relatedTarget.id != "menu"){
+        event.relatedTarget.style.background = "#FFFFE0";
+    }
+}
+
+function clickMenuItem(event) {
+    var color = "#FFDAB9";
+    if (event.type == "mousedown")
+        color = "#FFE4C4";
+    this.style.backgroundColor = color;
+}
+
+
 
 function loadingStyles(){
     var pageBody = document.getElementsByTagName("body");
     for(var key in styleObject.body){
         pageBody[0].style[key] = styleObject.body[key];
-    }    
-
-    
-var menu = document.getElementsByClassName("menu-content");
-menu.addEventListener('click', function(){
-    menu.style.display = styleObject["menu-content"].display;
-});
+    }   
+    var menu = document.getElementsByClassName("menu-content");
+    menu[0].style.display = "none";
+    var link = document.getElementsByTagName("a"); //element.get.... не работает
+    [].forEach.call(link, function(item){
+        item.style.background = "#FFFFE0";
+    });
 }
+
+function menuShow(){
+    var menu = document.getElementsByClassName("menu-content");
+    for(var key in styleObject["menu-content"]){
+        menu[0].style[key] = styleObject["menu-content"][key];
+    }
+    
+    var link = document.getElementsByTagName("a"); //element.get.... не работает
+    [].forEach.call(link, function(item){
+        item.addEventListener("mouseover", hoverMenuItem);
+        item.addEventListener("mousedown", clickMenuItem);
+        item.addEventListener("mouseup", clickMenuItem);
+    });
+}
+
+
 
 
 
