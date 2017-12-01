78f1b32fc24e47e183b17733841172f1743cceb1
epam <epam@local.rsu.edu.ru>
2017-12-01 19:04:53 +0300

    this v1

--- calculator.js
+++ calculator.js
@@ -1,5 +1,68 @@
-function calculator(){
+var Calculator = function(){
     var buffer = 0;
+    this.add = function(value1){
+        buffer += value1;
+        return function(value2){
+            if (value2){
+                return buffer += value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    //123
+    this.subtract = function(value1){
+        buffer -= value1;
+        return function(value2){
+            if (value2){
+                return buffer -= value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.multiply = function(value1){
+        buffer *= value1;
+        return function(value2){
+            if (value2){
+                return buffer *= value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.divide = function(value1){
+        buffer /= value1;
+        return function(value2){
+            if (value2){
+                return buffer /= value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.reset = function(){
+        return buffer = 0;
+    };
+    this.result = function(){
+        return buffer;
+    };
+}
+
+var calc = new Calculator();
+console.log(calc.add(5)(10));
+console.log(calc.add(5)());
+console.log(calc.result());
+console.log(calc.reset());
+
+
+/*
+function calculator(){
+    buffer = 0;
     return {
         add : function (value1){
             return function(value2){
@@ -46,4 +109,4 @@ function calculator(){
 
 var calc = new calculator();
 console.log(calc.deduct(10)(8));
-console.log(calc.divide(2)());
+console.log(calc.divide(2)());*/
