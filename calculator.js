dbc78ec1f7c516542698b18cdb37d20991263d36
Nadya <nadia_1492@mail.ru>
2017-12-03 19:36:18 +0300

    calculator v1 (final)

--- calculator.js
+++ calculator.js
@@ -1,215 +1,157 @@
-// var Calculator = function(){
-//     var buffer = 0;
-//     this.add = function(value1){
-//         buffer += value1;
-//         return function(value2){
-//             if (value2){
-//                 return buffer += value2;
-//             }
-//             else {
-//                 return buffer;
-//             }
-//         };
-//     };
-//     this.subtract = function(value1){
-//         buffer -= value1;
-//         return function(value2){
-//             if (value2){
-//                 return buffer -= value2;
-//             }
-//             else {
-//                 return buffer;
-//             }
-//         };
-//     };
-//     this.multiply = function(value1){
-//         buffer *= value1;
-//         return function(value2){
-//             if (value2){
-//                 return buffer *= value2;
-//             }
-//             else {
-//                 return buffer;
-//             }
-//         };
-//     };
-//     this.divide = function(value1){
-//         buffer /= value1;
-//         return function(value2){
-//             if (value2){
-//                 return buffer /= value2;
-//             }
-//             else {
-//                 return buffer;
-//             }
-//         };
-//     };
-//     this.reset = function(){
-//         return buffer = 0;
-//     };
-//     this.result = function(){
-//         return buffer;
-//     };
-// }
+var Calculator = function(){
+    var buffer = 0;
+    this.add = function(value1){
+        buffer += value1;
+        return function(value2){
+            if (value2){
+                return buffer = value1 + value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.subtract = function(value1){
+        buffer -= value1;
+        return function(value2){
+            if (value2){
+                return buffer = value1 - value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.multiply = function(value1){
+        buffer *= value1;
+        return function(value2){
+            if (value2){
+                return buffer = value1 * value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.divide = function(value1){
+        buffer /= value1;
+        return function(value2){
+            if (value2){
+                return buffer = value1 / value2;
+            }
+            else {
+                return buffer;
+            }
+        };
+    };
+    this.reset = function(){
+        return buffer = 0;
+    };
+    this.result = function(){
+        return buffer;
+    };
+}
 
-// var calc = new Calculator();
-// console.log(calc.add(5)(10));
-// console.log(calc.add(5)());
-// console.log(calc.result());
-// console.log(calc.reset());
+function init(){
+  var calc = new Calculator();
 
+  var contaner = document.getElementById("contaner");
+  var calcItem = document.createElement('div');
+  calcItem.setAttribute('class', 'calculator');
+  contaner.appendChild(calcItem);
+  var field = document.createElement("input");
+  field.setAttribute("type", "text");
+  field.setAttribute("placeholder", "0");
+  field.setAttribute("readonly", "true");
+  field.setAttribute("class", "field");
+  calcItem.appendChild(field);
 
-// function CoffeeMachine(power, capacity) {
-//     var waterAmount = 0;
-  
-//     this.waterAmount = function(amount) {
-//       // вызов без параметра, значит режим геттера, возвращаем свойство
-//       if (!arguments.length) return waterAmount;
-  
-//       // иначе режим сеттера
-//       if (amount < 0) {
-//         throw new Error("Значение должно быть положительным");
-//       }
-//       if (amount > capacity) {
-//         throw new Error("Нельзя залить воды больше, чем " + capacity);
-//       }
-  
-//       waterAmount = amount;
-//     };
-  
-//   }
-  
-//   var coffeeMachine = new CoffeeMachine(1000, 500);
-  
-//   // пример использования
-//   coffeeMachine.waterAmount(450);
-//   console.log( coffeeMachine.waterAmount() ); // 450
+  var buttons = new Array(9).fill("1");
+  buttons.forEach(function(elem, i){
+    buttons.fill((i+1).toString(), i, i+1);
+  });
+  buttons.splice(3, 0, "+");
+  buttons.splice(7, 0, "-");
+  buttons.push("*");
+  buttons.push(".");
+  buttons.push("C");
+  buttons.push("=");
+  buttons.push(":")
 
-//--------------------------------task 1-----------------------------------------------------------------------------
-// function User() {
-//     var firstName = "";
-//     var surname = "";
-//     this.getFullName = function(){
-//         if ((firstName+surname).trim() != ""){
-//             return "FirstName: " + firstName + ", Surname: " + surname;
-//         }
-//         else return "Please set the name."
-//     };
-//     this.setFirstName = function(fname){
-//         if (typeof fname == "string"){
-//             return firstName = fname;
-//         }
-//         else return "Please set correct first name."
-//     };
-//     this.setSurname = function(sname){
-//         if (typeof sname == "string"){
-//             return surname = sname;
-//         }
-//         else return "Please set correct surname."
-//     };
-//   }
-  
-//   var user = new User();
-//   user.setFirstName("");
-//   user.setSurname(" ");
-  
-//   console.log( user.getFullName() ); // Петя Иванов
-
-//----------------------------------------task 2------------------------------------------------------------------------------
-// function CoffeeMachine(power, capacity) {
-//     //..
-//     this.setWaterAmount = function(amount) {
-//       if (amount < 0) {
-//         throw new Error("Значение должно быть положительным");
-//       }
-//       if (amount > capacity) {
-//         throw new Error("Нельзя залить воды больше, чем " + capacity);
-//       }
-  
-//       waterAmount = amount;
-//     };
-  
-//     this.getWaterAmount = function() {
-//       return waterAmount;
-//     };
-
-//     this.getPower = function(){
-//         return power;
-//     }
-  
-//   }
-
-
-// function CoffeeMachine(power, capacity) {
-//     var waterAmount = 0;
-  
-//     var WATER_HEAT_CAPACITY = 4200;
-  
-//     function getTimeToBoil() {
-//       return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-//     }
-  
-//     this.setWaterAmount = function(amount) {
-//       if (amount < 0) {
-//         throw new Error("Значение должно быть положительным");
-//       }
-//       if (amount > capacity) {
-//         throw new Error("Нельзя залить больше, чем " + capacity);
-//       }
-  
-//       waterAmount = amount;
-//     };
-  
-//     function onReady() {
-//         console.log( 'Кофе готов!' );
-//     }
-  
-//     this.run = function() {
-//       setTimeout(onReady, getTimeToBoil());
-//     };
-
-//     this.addWater = function(amount){
-//         if (amount > 0 && amount <= (capacity - waterAmount)){
-//             return waterAmount += amount;
-//         }
-//         else throw new Error("something goes wrong");
-//         // this.setWaterAmount(waterAmount + amount);
-//     }
-  
-//   }
-
-//   var coffeeMachine = new CoffeeMachine(100000, 400);
-//   coffeeMachine.addWater(200);
-//   coffeeMachine.addWater(100);
-//   coffeeMachine.addWater(300); // Нельзя залить больше, чем 400
-//   coffeeMachine.run();
-
-
-
-  function CoffeeMachine(power, capacity) {
-    var waterAmount = 0;
-  
-    var WATER_HEAT_CAPACITY = 4200;
-  
-    function getTimeToBoil() {
-      return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+  var getResult = function(){
+    var operation = field.value.match(/[\-\:\*\+]/)[0];
+    var value1 = Number(field.value.slice(0, field.value.indexOf(operation)));
+    var value2 = Number(field.value.slice(field.value.indexOf(operation)+1));
+    if (value1 != "" && value2 != ""){
+      switch (operation){
+        case "+": 
+          field.value = calc.add(value1)(value2); 
+          break;
+        case "-": 
+          field.value = calc.subtract(value1)(value2); 
+          break;
+        case "*": 
+          field.value = calc.multiply(value1)(value2); 
+          break;
+        case ":": 
+          field.value = calc.divide(value1)(value2); 
+          break;
+      };
     }
-  
-    this.setWaterAmount = function(amount) {
-      // ... проверки пропущены для краткости
-      waterAmount = amount;
-    };
-  
-    this.getWaterAmount = function(amount) {
-      return waterAmount;
-    };
-  
-    function onReady() {
-        alert( 'Кофе готов!' );
-      }
-  
-    this.run = function() {
-      setTimeout(onReady, getTimeToBoil());
-    };
-  
   }
+
+  var reset = function(){
+    calc.reset;
+    field.value = "";
+  }
+  
+  buttons.forEach(function(elem, i){
+    var button = document.createElement("button");
+    button.innerHTML = elem;
+    button.setAttribute("class", "btn");
+    switch (elem){
+      case "=":
+        button.addEventListener("click", getResult);
+        break;
+      case "C":
+        button.addEventListener("click", reset);
+        break;
+      case ".":
+        button.addEventListener("click", setDot);
+        break;
+      default:
+        button.addEventListener("click", setExpression);
+    }
+    calcItem.appendChild(button);
+  });
+
+  
+  function setExpression(){
+    var field = this.parentNode.querySelector(".field");
+    if (field.value != "" && field.value.match(/[\-\:\*\+]/) != null && this.innerHTML.match(/[\-\:\*\+]/) != null){
+      console.log(field.value.match(/[\-\:\*\+]/)[0]);
+      getResult();
+      field.value += this.innerHTML;
+    }
+    else if (!(this.innerHTML.match(/[\-\:\*\+]/) != null && field.value == ""))
+      field.value += this.innerHTML;
+  }
+
+  function setDot(){
+    var operation = field.value.match(/[\-\:\*\+]/);
+    if (operation != null){
+      var value2 = field.value.slice(field.value.indexOf(operation[0])+1);
+      if (value2.indexOf(".") == -1){
+        field.value += this.innerHTML;
+      }
+    }
+    else if (field.value.indexOf(".") == -1){
+      field.value += this.innerHTML;
+    }
+  }
+
+}
+
+
+
+
