a40f5456bf20ded446a1e5ff49fddbe73465335d
Nadya <nadia_1492@mail.ru>
2017-12-04 22:40:25 +0300

    calculator v2. Added negatives and zero

--- calculator.js
+++ calculator.js
@@ -75,13 +75,27 @@ function init(){
   buttons.push("*");
   buttons.push(".");
   buttons.push("C");
-  buttons.push("=");
+  buttons.push("0");
   buttons.push(":")
+  buttons.push("=");
+
 
   var getResult = function(){
-    var operation = field.value.match(/[\-\:\*\+]/)[0];
-    var value1 = Number(field.value.slice(0, field.value.indexOf(operation)));
-    var value2 = Number(field.value.slice(field.value.indexOf(operation)+1));
+    var value1;
+    var value2;
+    var val;
+    if (field.value[0]=="-"){
+        val = field.value.slice(1);
+        value1 = -1;
+    }
+    else{
+        val = field.value;
+        value1 = 1;
+    }
+    var operation = val.match(/[\-\:\*\+]/)[0];
+    value1 *= Number(val.slice(0, val.indexOf(operation)));
+    value2 = Number(val.slice(val.indexOf(operation)+1));
+    
     if (value1 != "" && value2 != ""){
       switch (operation){
         case "+": 
@@ -111,6 +125,7 @@ function init(){
     button.setAttribute("class", "btn");
     switch (elem){
       case "=":
+        button.style["width"] = "300px";
         button.addEventListener("click", getResult);
         break;
       case "C":
@@ -128,12 +143,13 @@ function init(){
   
   function setExpression(){
     var field = this.parentNode.querySelector(".field");
-    if (field.value != "" && field.value.match(/[\-\:\*\+]/) != null && this.innerHTML.match(/[\-\:\*\+]/) != null){
-      console.log(field.value.match(/[\-\:\*\+]/)[0]);
-      getResult();
-      field.value += this.innerHTML;
+    if (field.value != "" 
+        && ((field.value[0]=="-")?field.value.slice(1):field.value).match(/[\-\:\*\+]/) != null 
+        && this.innerHTML.match(/[\-\:\*\+]/) != null){
+        getResult();
+        field.value += this.innerHTML;
     }
-    else if (!(this.innerHTML.match(/[\-\:\*\+]/) != null && field.value == ""))
+    else if (!(this.innerHTML.match(/[\:\*\+]/) != null && field.value == ""))
       field.value += this.innerHTML;
   }
 
