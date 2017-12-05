8c221a92a194b3a8526a49da8872e7b4e561414c
epam <epam@local.rsu.edu.ru>
2017-12-05 21:22:25 +0300

    v3

--- calculator.js
+++ calculator.js
@@ -76,69 +76,129 @@ function init(){
   buttons.push(".");
   buttons.push("C");
   buttons.push("0");
-  buttons.push(":")
+  buttons.push("/")
   buttons.push("=");
 
-
-  var getResult = function(){
-    var value1;
-    var value2;
-    var val;
-    if (field.value[0]=="-"){
-        val = field.value.slice(1);
-        value1 = -1;
-    }
-    else{
-        val = field.value;
-        value1 = 1;
-    }
-    var operation = val.match(/[\-\:\*\+]/)[0];
-    value1 *= Number(val.slice(0, val.indexOf(operation)));
-    value2 = Number(val.slice(val.indexOf(operation)+1));
-    
-    if (value1 != "" && value2 != ""){
-      switch (operation){
-        case "+": 
-          field.value = calc.add(value1)(value2); 
-          break;
-        case "-": 
-          field.value = calc.subtract(value1)(value2); 
-          break;
-        case "*": 
-          field.value = calc.multiply(value1)(value2); 
-          break;
-        case ":": 
-          field.value = calc.divide(value1)(value2); 
-          break;
-      };
-    }
-  }
-
-  var reset = function(){
-    calc.reset;
-    field.value = "";
-  }
-  
-  buttons.forEach(function(elem, i){
+    buttons.forEach(function(elem, i){
     var button = document.createElement("button");
     button.innerHTML = elem;
     button.setAttribute("class", "btn");
     switch (elem){
       case "=":
         button.style["width"] = "300px";
-        button.addEventListener("click", getResult);
+        // button.addEventListener("click", getResult);
+        button.setAttribute("class", "equal");
         break;
       case "C":
-        button.addEventListener("click", reset);
+        // button.addEventListener("click", reset);
+        button.setAttribute("class", "clear");
         break;
       case ".":
-        button.addEventListener("click", setDot);
+        // button.addEventListener("click", setDot);
+        button.setAttribute("class", "dot");
+        break;
+      case "+":
+        button.setAttribute("class", "add");
+        break;
+      case "-":
+        button.setAttribute("class", "subtract");
+        break;
+      case "*":
+        button.setAttribute("class", "multiple");
+        break;
+      case "/":
+        button.setAttribute("class", "divide");
         break;
       default:
-        button.addEventListener("click", setExpression);
+      button.setAttribute("class", "number");
     }
     calcItem.appendChild(button);
   });
+
+  // controller
+
+  var StartState = function(){
+    return {
+      items : ["", ""],
+      currentItem : 0,
+      currentOperation : null,
+      currentAction : true //вводим цифры
+    };
+  };
+
+  var state = new StartState();
+
+  document.querySelectorAll(".calculator").forEach(function(el){
+    el.addEventListener("click", function(){
+      if (document.querySelector(".btn .dot")){
+        console.log("www");
+      }
+    })
+  });
+
+  var getResult = function(){
+    // var value1;
+    // var value2;
+    // var val;
+    // if (field.value[0]=="-"){
+    //     val = field.value.slice(1);
+    //     value1 = -1;
+    // }
+    // else{
+    //     val = field.value;
+    //     value1 = 1;
+    // }
+    // var operation = val.match(/[\-\:\*\+]/)[0];
+    // value1 *= Number(val.slice(0, val.indexOf(operation)));
+    // value2 = Number(val.slice(val.indexOf(operation)+1));
+    
+    // if (value1 != "" && value2 != ""){
+    //   switch (operation){
+    //     case "+": 
+    //       field.value = calc.add(value1)(value2); 
+    //       break;
+    //     case "-": 
+    //       field.value = calc.subtract(value1)(value2); 
+    //       break;
+    //     case "*": 
+    //       field.value = calc.multiply(value1)(value2); 
+    //       break;
+    //     case ":": 
+    //       field.value = calc.divide(value1)(value2); 
+    //       break;
+    //   };
+    // }
+  }
+
+  var reset = function(){
+    calc.reset;
+    field.value = "";
+    state = new StartState();
+  }
+
+
+  document.querySelector
+  
+  // buttons.forEach(function(elem, i){
+  //   var button = document.createElement("button");
+  //   button.innerHTML = elem;
+  //   button.setAttribute("class", "btn");
+  //   switch (elem){
+  //     case "=":
+  //       button.style["width"] = "300px";
+  //       button.addEventListener("click", getResult);
+  //       break;
+  //     case "C":
+  //       button.addEventListener("click", reset);
+  //       break;
+  //     case ".":
+  //       button.addEventListener("click", setDot);
+  //       break;
+  //     default:
+  //       button.addEventListener("click", setExpression);
+  //   }
+  //   calcItem.appendChild(button);
+  // });
 
   
   function setExpression(){
@@ -153,18 +213,18 @@ function init(){
       field.value += this.innerHTML;
   }
 
-  function setDot(){
-    var operation = field.value.match(/[\-\:\*\+]/);
-    if (operation != null){
-      var value2 = field.value.slice(field.value.indexOf(operation[0])+1);
-      if (value2.indexOf(".") == -1){
-        field.value += this.innerHTML;
-      }
-    }
-    else if (field.value.indexOf(".") == -1){
-      field.value += this.innerHTML;
-    }
-  }
+  // function setDot(){
+  //   var operation = field.value.match(/[\-\:\*\+]/);
+  //   if (operation != null){
+  //     var value2 = field.value.slice(field.value.indexOf(operation[0])+1);
+  //     if (value2.indexOf(".") == -1){
+  //       field.value += this.innerHTML;
+  //     }
+  //   }
+  //   else if (field.value.indexOf(".") == -1){
+  //     field.value += this.innerHTML;
+  //   }
+  // }
 
 }
 
