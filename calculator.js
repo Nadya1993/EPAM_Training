3065046e7cd736790bfee75da1db3c43538207aa
Nadya <nadia_1492@mail.ru>
2017-12-13 21:57:27 +0300

    calculator v6. Final version

--- calculator.js
+++ calculator.js
@@ -52,16 +52,11 @@ var Calculator = function(){
     };
 }
 
-function init(){
-
-  //new calculator interface
+//new calculator interface
+function createCalc(){
   var container = document.getElementById("container");
   var calcItem = document.createElement('div');
   calcItem.setAttribute('class', 'calculator');
-
-  var id = document.getElementsByClassName("calculator").length;
-  calcItem.setAttribute('id', id+1);
-
   container.appendChild(calcItem);
   var field = document.createElement("input");
   field.setAttribute("type", "text");
@@ -69,7 +64,7 @@ function init(){
   field.setAttribute("readonly", "true");
   field.setAttribute("class", "field");
   calcItem.appendChild(field);
-
+  
   var buttons = new Array(9).fill("1");
   buttons.forEach(function(elem, i){
     buttons.fill((i+1).toString(), i, i+1);
@@ -82,7 +77,7 @@ function init(){
   buttons.push("0");
   buttons.push("/")
   buttons.push("=");
-
+  
   buttons.forEach(function(elem, i){
     var button = document.createElement("button");
     button.innerHTML = elem;
@@ -114,9 +109,11 @@ function init(){
     }
     calcItem.appendChild(button);
   });
-
- // controller
-  var StartState = function(){ 
+  return calcItem;
+  };
+  
+// controller
+var startState = function(){ 
     return {
       items : ["", ""],
       currentItem : 0,
@@ -124,97 +121,95 @@ function init(){
       currentAction : true, //вводим цифры, false - операция
       hasDot : false //следим за наличием точки
     };
-  };
+};
 
-var state = new StartState();
-//new calculator
-var calc = new Calculator();
-
-  function debounce(event){ //!!!
+function calcEntity(){ 
+  var state = startState();
+  var calc = new Calculator();
+  return function(){
     var fieldInput = event.target.parentNode.querySelector(".field");
 
     function equal(){
-        if (state.items[0] != "" && state.items[1] != ""){
-            state.currentOperation(state.items[1]);
-            fieldInput.value = calc.result();
-            state.items[0] = calc.result();
-            state.items[1] = "";
-            state.currentOperation = null;
-            state.currentAction = true;
-            state.currentItem = 0;
-          };
-    };
-
-    function setOperationState(){
-        fieldInput.value += event.target.innerHTML;
-        state.currentItem = 1;
-        state.currentAction = false;
-        state.hasDot = false;
-    };
-
-    switch (event.target.className){
-        case "btn number":
-          fieldInput.value += event.target.innerHTML;
-          state.items[state.currentItem] += event.target.innerHTML;
+      if (state.items[0] != "" && state.items[1] != ""){
+          state.currentOperation(state.items[1]);
+          fieldInput.value = calc.result();
+          state.items[0] = calc.result();
+          state.items[1] = "";
+          state.currentOperation = null;
           state.currentAction = true;
-          break;
-        case "btn add":
-          equal();
-          if (state.items[0] != "" && state.currentAction){
-            setOperationState();
-            state.currentOperation = calc.add(state.items[0]);
-          };
-          break;
-        case "btn subtract":
-          equal();
-          if (state.currentAction){
-            setOperationState();
-            if (state.items[0] === "") {
-                state.items[0] = "-";
-                state.currentItem = 0;
-            };
-            state.currentOperation = calc.subtract(state.items[0]);
-          };
-          break;
-        case "btn multiply":
-          equal();
-          if (state.items[0] != "" && state.currentAction){
-            setOperationState();
-            state.currentOperation = calc.multiply(state.items[0]);
-          };
-          break;
-        case "btn divide":
-          equal();
-          if (state.items[0] != "" && state.currentAction){
-            setOperationState();
-            state.currentOperation = calc.divide(state.items[0]);
-          };
-          break;
-        case "btn equal":
-          equal();
-          break;
-        case "btn clear":
-          calc.reset();
-          fieldInput.value = "";
-          state = new StartState();
-          break;
-        case "btn dot":
-          if (state.hasDot == false){
-              state.hasDot = true;
-              state.items[state.currentItem] += event.target.innerHTML;
-              fieldInput.value += event.target.innerHTML;
-            break;
-          }
-      }
+          state.currentItem = 0;
+        };
+  };
 
+  function setOperationState(){
+      fieldInput.value += event.target.innerHTML;
+      state.currentItem = 1;
+      state.currentAction = false;
+      state.hasDot = false;
+  };
+
+  switch (event.target.className){
+      case "btn number":
+        fieldInput.value += event.target.innerHTML;
+        state.items[state.currentItem] += event.target.innerHTML;
+        state.currentAction = true;
+        break;
+      case "btn add":
+        equal();
+        if (state.items[0] != "" && state.currentAction){
+          setOperationState();
+          state.currentOperation = calc.add(state.items[0]);
+        };
+        break;
+      case "btn subtract":
+        equal();
+        if (state.currentAction){
+          setOperationState();
+          if (state.items[0] === "") {
+              state.items[0] = "-";
+              state.currentItem = 0;
+          };
+          state.currentOperation = calc.subtract(state.items[0]);
+        };
+        break;
+      case "btn multiply":
+        equal();
+        if (state.items[0] != "" && state.currentAction){
+          setOperationState();
+          state.currentOperation = calc.multiply(state.items[0]);
+        };
+        break;
+      case "btn divide":
+        equal();
+        if (state.items[0] != "" && state.currentAction){
+          setOperationState();
+          state.currentOperation = calc.divide(state.items[0]);
+        };
+        break;
+      case "btn equal":
+        equal();
+        break;
+      case "btn clear":
+        calc.reset();
+        fieldInput.value = "";
+        state = startState();
+        break;
+      case "btn dot":
+        if (state.hasDot == false){
+            state.hasDot = true;
+            state.items[state.currentItem] += event.target.innerHTML;
+            fieldInput.value += event.target.innerHTML;
+        }
+        break;
+    }
   }
+};
 
-  document.querySelectorAll(".calculator").forEach(function(el, i, arr){
-    event.target.addEventListener("click", debounce);
-  });
-
+function init(){
+  var newCalc = createCalc();
+  var newCalcEntity = calcEntity();
+  newCalc.addEventListener("click", newCalcEntity);
 }
-
 
 
 
