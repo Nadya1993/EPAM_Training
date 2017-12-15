435ab461b2a114665169068422d6da1df133211c
epam <epam@local.rsu.edu.ru>
2017-12-15 21:23:44 +0300

    v1

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function loadMap() {
+    var xhr = new XMLHttpRequest();
 
-console.log("Hi test!")
+    xhr.open('GET', 'http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAdrZuFXlo3OoE2XXEH5d0smdoPhE6EKXk', true);
+    xhr.send();
+
+    if (xhr.status != 200) {
+      // обработать ошибку
+      alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+    } else {
+      // вывести результат
+      alert(xhr.responseText);
+    }
+  }
 
