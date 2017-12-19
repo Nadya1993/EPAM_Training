8de1e3a5eea8c893b73849a7a8dec385aab03eeb
epam <epam@local.rsu.edu.ru>
2017-12-19 20:54:22 +0300

    added model

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
 //AIzaSyCHQreOyVTeIunKEZIFzCCCrrTdO_Af8hk
-
-function loadMap() {
-    var searchQuery = {
-      city : document.querySelector("#city").value,
-      street : document.querySelector("#street").value,
-      structure : document.querySelector("#structure").value
-    };
-
-    localStorage.setItem('myCat', 'Tom');
-
-    jQuery.ajax({
-      url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+searchQuery.city+',+'+searchQuery.street+',+'+searchQuery.structure+'&key=AIzaSyCHQreOyVTeIunKEZIFzCCCrrTdO_Af8hk',
-      type: "GET",
-      contentType: 'application/x-www-form-urlencoded; charset=UTF-8', 
-      success: function(resultData) {
-        if (resultData.status == "ZERO_RESULTS") {
-          alert ("Please enter a correct address data!")
+function ListAction() {
+  return {
+    add : function(item){
+      localStorage.setItem(item, 0);
+    },
+    check : function(item){
+      localStorage.item = 1;
+    },
+    uncheck : function(item){
+      localStorage.item = 0;
+    },
+    selectAll : function(){
+      for (key in localStorage){
+        localStorage[key] = 1;
+      }
+    },
+    unselectAll : function(){
+      for (key in localStorage){
+        localStorage[key] = 0;
+      }
+    },
+    showActive : function(){
+      var active = {};
+      for (key in localStorage){
+        if (localStorage[key] == 1){
+          active[key] = 1;
         }
-        else {
-          document.querySelector(".result").innerHTML = "https://www.google.ru/maps/@" + resultData.results[0].geometry.location.lat + "," + resultData.results[0].geometry.location.lng + ",7.75z";
-          document.querySelector(".result").href = "https://www.google.ru/maps/@" + resultData.results[0].geometry.location.lat + "," + resultData.results[0].geometry.location.lng + ",7.75z";
-          // "Latitude: " + resultData.results[0].geometry.location.lat.toFixed(2)  
-          // + "; Longitude: " + resultData.results[0].geometry.location.lng.toFixed(2);
-        };
-      },
-      error : function(jqXHR, textStatus, errorThrown) {
-        alert("Something goes wrong: " + textStatus);
-      },
-
-      timeout: 12000
-  });
+      };
+      return active;
+    },
+    showInActive : function(){
+      var inActive = {};
+      for (key in localStorage){
+        if (localStorage[key] == 0){
+          inActive[key] = 0;
+        }
+      };
+      return inActive;
+    },
 
   }
+}
