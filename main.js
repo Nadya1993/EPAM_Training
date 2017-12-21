8c51ee6f36ec6161352a47ac36acd66ca5f953c7
epam <epam@local.rsu.edu.ru>
2017-12-21 21:12:34 +0300

    v2

--- main.js
+++ main.js
@@ -1,75 +1,62 @@
 
-  // var result;
-function getUuid() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'uuid', true);
-  xhr.onreadystatechange = function(result) {
-    if (xhr.readyState != 4) return;
-    if (xhr.status != 200) {
-      alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-      return;
+function uuid() {
+  var uuid = "", i, random;
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+
+    if (i == 8 || i == 12 || i == 16 || i == 20) {
+      uuid += "-"
     }
-    //alert(xhr.responseText)
-    // result = xhr.responseText;
-    localStorage.setItem("toDoList", xhr.responseText);
+    uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
   }
-  xhr.send(null);
+  return uuid;
 }
 
-getUuid()
 
-
-
-
-
-// function ListAction() {
-//   localStorage.setItem("toDoList", '[]');
-//   return {
-//     add : function(item){
-//       console.log(JSON.stringify({"id" : getUuid(), "title" : item, "status" : 0}));
-//       getUuid();
-//       console.log("1"+result)
-//       JSON.parse(localStorage["toDoList"]).push(JSON.stringify({"id" : getUuid(), "title" : item, "status" : 0}));
-//     }//,
-//     // check : function(item){
-//     //   localStorage.item = 1;
-//     // },
-//     // uncheck : function(item){
-//     //   localStorage.item = 0;
-//     // },
-//     // selectAll : function(){
-//     //   for (key in localStorage){
-//     //     localStorage[key] = 1;
-//     //   }
-//     // },
-//     // unselectAll : function(){
-//     //   for (key in localStorage){
-//     //     localStorage[key] = 0;
-//     //   }
-//     // },
-//     // showActive : function(){
-//     //   var active = {};
-//     //   for (key in localStorage){
-//     //     if (localStorage[key] == 1){
-//     //       active[key] = 1;
-//     //     }
-//     //   };
-//     //   return active;
-//     // },
-//     // showInActive : function(){
-//     //   var inActive = {};
-//     //   for (key in localStorage){
-//     //     if (localStorage[key] == 0){
-//     //       inActive[key] = 0;
-//     //     }
-//     //   };
-//     //   return inActive;
-//     // }
-
-//   }
-// }
+function ListAction() {
+  localStorage.setItem("toDoList", '[]');
+  var itemArr = JSON.parse(localStorage["toDoList"]);
+  return {
+    add : function(item){
+      alert(item)
+      itemArr.push({"id" : uuid(), "title" : item, "status" : 0});
+      localStorage.setItem("toDoList", JSON.stringify(itemArr));
+    },
+    selectAll : function(){
+      itemArr.forEach(function(element) {
+        element.status = 1;
+      });
+    },
+    unselectAll : function(){
+      itemArr.forEach(function(element) {
+        element.status = 0;
+      });
+    }
+  }
+}
 
 // var elem = new ListAction();
 // elem.add("qqq");
-// elem.add("yyy")
-// console.log
+// elem.add("222");
+
+
+
+function deb(func, value){
+   return function(){
+     alert(func)
+     alert(value)
+    return func(value);
+   }
+}
+
+
+function init(){
+  var elem = new ListAction();
+  function f(){
+    alert(document.querySelector(".input").value)
+  }
+  var askingDebounce = deb(elem.add, document.querySelector(".input").value);
+  document.querySelector(".add").addEventListener("click", askingDebounce);
+  document.querySelector(".add").addEventListener("click", f);
+}
+
